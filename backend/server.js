15ed1bb494936a7f308b39ce3f49da0e90b8a803
L_K_B <lalitbansal8322@gmail.com>
2026-02-22 16:25:35 +0530

    add render url

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
   cors({
-    origin:["http://localhost:5173", "http://localhost:5174"],
+    origin:["https://lms-yce2.onrender.com"],
     credentials: true,
   })
 );
